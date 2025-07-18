4599eb8e610bcef7f1b4fcef58ebc5e0e7be8b5b
Chen Wei Lun <weilunchen210@gmail.com>
2025-07-18 12:02:40 +0800

    added remove counter function

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const main = document.getElementById("counter-grids")
 
 document.getElementById("add-counter").addEventListener("click", addCounter)
+let divCount = 0;
 
 function addCounter(){
+    divCount++;
     const counterContainer = document.createElement("div")
+    counterContainer.id = `div${divCount}`
     counterContainer.innerHTML = `<div class="counter-container">
                     <div class="container-actions">
-                        <img src="https://img.icons8.com/?size=100&id=11705&format=png&color=FFFFFF" class="icon"/>
+                        <img onclick=remove(this) src="https://img.icons8.com/?size=100&id=11705&format=png&color=FFFFFF" class="icon"/>
                     </div>
                     <h3>Counter 1</h3>
                     <h1>34</h1>
@@ -16,4 +19,9 @@ function addCounter(){
     main.appendChild(counterContainer)
 }
 
+function remove(img){
+    const parentDiv = img.closest('.counter-container');
+    const grandparentDiv = parentDiv.parentElement;
+    grandparentDiv.remove();
 
+}
