f0d574d7e62b5a3ca43a897d5346878b2ebf2655
Chen Wei Lun <weilunchen210@gmail.com>
2025-07-18 11:22:08 +0800

    add counter container function added

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+const main = document.getElementById("counter-grids")
+
+document.getElementById("add-counter").addEventListener("click", addCounter)
+
+function addCounter(){
+    const counterContainer = document.createElement("div")
+    counterContainer.innerHTML = `<div class="counter-container">
+                    <div class="container-actions">
+                        <img src="https://img.icons8.com/?size=100&id=11705&format=png&color=FFFFFF" class="icon"/>
+                    </div>
+                    <h3>Counter 1</h3>
+                    <h1>34</h1>
+                    <button>Click</button>
+                </div>`
+
+    main.appendChild(counterContainer)
+}
 
 
